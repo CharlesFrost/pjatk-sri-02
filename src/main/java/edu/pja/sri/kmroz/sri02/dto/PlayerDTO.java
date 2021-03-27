6c77010e6a586c16f89428a43a5a6cc0517c9b55
Karol <karolmroz@protonmail.com>
2021-03-27 01:10:35 +0100

    validations fix

--- src/main/java/edu/pja/sri/kmroz/sri02/dto/PlayerDTO.java
+++ src/main/java/edu/pja/sri/kmroz/sri02/dto/PlayerDTO.java
@@ -4,9 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.PastOrPresent;
 import javax.validation.constraints.Size;
